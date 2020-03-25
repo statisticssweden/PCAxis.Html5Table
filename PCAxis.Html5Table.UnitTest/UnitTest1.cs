390ae4c58302ea1a31f1c69483fbde42c3350153
cklinton <60175636+cklinton@users.noreply.github.com>
2020-03-25 15:09:48 +0100

    A first commit

---
+++ PCAxis.Html5Table.UnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PCAxis.Html5Table.UnitTest
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
